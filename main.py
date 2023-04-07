e9c5fccbef361e911182e083f7906efb5ec779f9
DISvep <321861bro@gmail.com>
2023-04-07 17:20:19 +0300

    ball

--- main.py
+++ main.py
@@ -1,9 +1,58 @@
 import pygame
+import random
 
 pygame.init()
 
 scr = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("Ping-Pong")
+
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((10, 10))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+        self.rect.x = 250
+        self.rect.y = 250
+        self.start = True
+        self.right = False
+        self.up = False
+        self.speed = 1.5
+
+    def draw(self):
+        pygame.draw.circle(scr, (255, 255, 255), (self.rect.x, self.rect.y), 5, 2)
+        # scr.blit(self.surf, (self.rect.x, self.rect.y))
+
+    def update(self):
+        if self.start:
+            if random.randint(1, 2) == 1:
+                self.right = True
+            if random.randint(1, 2) == 1:
+                self.up = True
+            self.start = False
+        if self.rect.x >= 500 or self.rect.x <= 0:
+            self.start = True
+            self.rect.x, self.rect.y = 250, 250
+        if self.rect.y >= 500 or self.rect.y <= 0:
+            self.up = not self.up
+        if self.right:
+            if self.up:
+                self.rect.x += self.speed
+                self.rect.y += self.speed
+            else:
+                self.rect.x += self.speed
+                self.rect.y -= self.speed
+        else:
+            if self.up:
+                self.rect.x -= self.speed
+                self.rect.y += self.speed
+            else:
+                self.rect.x -= self.speed
+                self.rect.y -= self.speed
+
+    def bounce(self):
+        self.right = not self.right
 
 
 class Wall(pygame.sprite.Sprite):
@@ -30,6 +79,7 @@ class Wall(pygame.sprite.Sprite):
 
 
 plr = Wall(True)
+ball = Ball()
 
 clock = pygame.time.Clock()
 game = True
@@ -46,6 +96,11 @@ while game:
     if key[pygame.K_s]:
         plr.down()
 
+    if pygame.Rect.colliderect(plr.rect, ball.rect):
+        ball.bounce()
+
     plr.draw()
+    ball.draw()
+    ball.update()
     pygame.display.update()
     clock.tick(60)
