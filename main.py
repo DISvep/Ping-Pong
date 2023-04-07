429ee39906d2d92d3af2f9cfb80ef917882dde6f
DISvep <321861bro@gmail.com>
2023-04-07 18:33:56 +0300

    score

--- main.py
+++ main.py
@@ -3,10 +3,39 @@ import random
 
 pygame.init()
 
-scr = pygame.display.set_mode((500, 500))
+scr = pygame.display.set_mode((500, 600))
 pygame.display.set_caption("Ping-Pong")
 
 last = 0
+slast = 0
+
+
+class Text():
+    def __init__(self):
+        self.font = pygame.font.SysFont('Comic Sans MS', 30)
+        self.score_ai = 0
+        self.score_plr = 0
+        self.times = 0
+        self.s1 = self.font.render(str(self.times), False, (255, 255, 255))
+        self.s2 = self.font.render(str(self.score_ai), False, (255, 255, 255))
+        self.s3 = self.font.render(str(self.score_plr), False, (255, 255, 255))
+
+    def draw(self):
+        self.s1 = self.font.render(str(self.times), False, (255, 255, 255))
+        self.s2 = self.font.render(str(self.score_ai), False, (255, 255, 255))
+        self.s3 = self.font.render(str(self.score_plr), False, (255, 255, 255))
+        scr.blit(self.s1, (250, 40))
+        scr.blit(self.s2, (40, 40))
+        scr.blit(self.s3, (460, 40))
+
+    def ai(self):
+        self.score_ai += 1
+
+    def plr(self):
+        self.score_plr += 1
+
+    def time(self):
+        self.times += 1
 
 
 class Ball(pygame.sprite.Sprite):
@@ -16,7 +45,7 @@ class Ball(pygame.sprite.Sprite):
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect()
         self.rect.x = 250
-        self.rect.y = 250
+        self.rect.y = 350
         self.start = True
         self.right = False
         self.up = False
@@ -33,10 +62,15 @@ class Ball(pygame.sprite.Sprite):
             if random.randint(1, 2) == 1:
                 self.up = True
             self.start = False
-        if self.rect.x >= 500 or self.rect.x <= 0:
+        if self.rect.x >= 500:
             self.start = True
-            self.rect.x, self.rect.y = 250, 250
-        if self.rect.y >= 500 or self.rect.y <= 0:
+            self.rect.x, self.rect.y = 250, 350
+            points.ai()
+        elif self.rect.x <= 0:
+            self.start = True
+            self.rect.x, self.rect.y = 250, 350
+            points.plr()
+        if self.rect.y >= 600 or self.rect.y <= 100:
             self.up = not self.up
         if self.right:
             if self.up:
@@ -73,11 +107,11 @@ class Wall(pygame.sprite.Sprite):
         scr.blit(self.surf, (self.rect.x, self.rect.y))
 
     def up(self):
-        if self.rect.y > 0:
+        if self.rect.y > 100:
             self.rect.y -= 10
 
     def down(self):
-        if self.rect.y < 450:
+        if self.rect.y < 550:
             self.rect.y += 10
 
     def update(self, ballRect):
@@ -90,6 +124,7 @@ class Wall(pygame.sprite.Sprite):
 plr = Wall(True)
 AI = Wall(False)
 ball = Ball()
+points = Text()
 
 clock = pygame.time.Clock()
 game = True
@@ -108,6 +143,10 @@ while game:
 
     cur = pygame.time.get_ticks()
 
+    if cur - slast >= 1000:
+        points.time()
+        slast = cur
+
     if pygame.Rect.colliderect(plr.rect, ball.rect) or pygame.Rect.colliderect(AI.rect, ball.rect):
         if cur - last >= 1000:
             ball.bounce()
@@ -118,5 +157,6 @@ while game:
     AI.update(ball)
     ball.draw()
     ball.update()
+    points.draw()
     pygame.display.update()
     clock.tick(60)
